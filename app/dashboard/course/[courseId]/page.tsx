332ed81c0a25ccf3ec6e57032eee7302978c123e
Battugs Choijil <battugs@fibo.cloud>
2024-11-01 17:41:05 +0800

    [sdsd]

--- app/dashboard/course/[courseId]/page.tsx
+++ app/dashboard/course/[courseId]/page.tsx
@@ -1,4 +1,3 @@
-import { LoadingSpinner } from "@/components/ui/loading";
 import { auth } from "@/integration/auth-handler";
 import { redirect } from "next/navigation";
 import CourseClient from "./_components";
@@ -12,17 +11,16 @@ interface PageProps {
 async function CourseIdPage({ params }: PageProps) {
   const session = await auth();
 
-  if (!session) {
-    return <LoadingSpinner />;
+  if (!session?.user?.id) {
+    redirect("/auth/login");
   }
 
-  const { courseId } = await params;
+  const { courseId } = params;
 
   if (courseId !== "1") {
     redirect("/dashboard/course/1");
   }
 
-  console.log(typeof courseId, "dddddd");
   const sampleData = [
     { date: "2020-10-23", listen: 10, reading: 20, writing: 30, speaking: 40 },
     { date: "2020-10-24", listen: 15, reading: 25, writing: 35, speaking: 45 },
